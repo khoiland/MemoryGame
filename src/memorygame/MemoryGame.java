f17ee6a7ec003be3e8477b185cca90309adc8dd9
Niethy631 <bra12020@byui.edu>
2013-10-03 11:48:34 -0700

    Commit

--- src/memorygame/MemoryGame.java
+++ src/memorygame/MemoryGame.java
@@ -6,11 +6,16 @@ package memorygame;
 
 /**
  *
- * @author Kellie
+ * @author Kellie/Melanie
  */
 public class MemoryGame {
-
-    /**
+        String name; //first name
+        Number noplayers; //number of players
+        String instructions = "The goal of the game: At the end of the game, \"n\n"
+        + "everyone adds up the number of match. Each match is worth one \n" 
+        + "point. The person with the most matches is the winner.\n"
+        + "GoodLuck!!!\n\n";
+        /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
